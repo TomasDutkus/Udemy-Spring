7922d59dc8e380eeb7cedb321f474d8810eb303a
Tomas Dutkus <39946082+TomasDutkus@users.noreply.github.com>
2019-05-07 15:52:53 +0300

    Add files via upload

---
+++ 22-jpa-cruddemo/src/main/java/com/tomasdutkus/springboot/cruddemo/service/EmployeServiceImpl.java
@@ -0,0 +1,45 @@
+package com.tomasdutkus.springboot.cruddemo.service;
+
+import com.tomasdutkus.springboot.cruddemo.dao.EmployeeDAO;
+import com.tomasdutkus.springboot.cruddemo.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class EmployeServiceImpl implements EmployeeService {
+
+    private EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeServiceImpl(@Qualifier ("employeeDAOJpaImpl")EmployeeDAO theEmployeeDao) {
+        employeeDAO = theEmployeeDao;
+    }
+
+    @Override
+    @Transactional
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+
+    @Override
+    @Transactional
+    public Employee findById(int theId) {
+        return employeeDAO.findById(theId);
+    }
+
+    @Override
+    @Transactional
+    public void save(Employee theEmployee) {
+        employeeDAO.save(theEmployee);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int theId) {
+        employeeDAO.deleteById(theId);
+    }
+}
---
+++ 22-jpa-cruddemo/src/main/java/com/tomasdutkus/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.tomasdutkus.springboot.cruddemo.service;
+
+import com.tomasdutkus.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> findAll();
+
+    public Employee findById(int theId);
+
+    public void save(Employee theEmployee);
+
+    public void deleteById(int theId);
+}
